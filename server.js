302bef5aae8e4848e10a411a4192649eda9b078c
yo230899 <agustinustheoo@gmail.com>
2018-08-16 04:46:48 +0700

    more server fixes (added process.env.PORT) :)

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const hostname = 'localhost';
-const port = '3000' || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
