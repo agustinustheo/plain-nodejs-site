675559493a96db8cc8614e6def9840220cd8e7d5
agustinustheo <agustinustheoo@gmail.com>
2018-08-22 12:45:18 +0700

    added profile page

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const pg = require('pg');
+const conString = "postgres://dilionagungjaya:password@localhost:5432/dilionagungjaya";
+const client = new pg.Client(conString);
 
 const hostname = 'localhost';
 const port = process.env.PORT || 3000;
+  
+let namakategori = [];
+let loop = 0;
+async function x() {
+  await client.connect();
+  const res = await client.query("SELECT namakategori FROM msKategori");
+  //fired after last row is emitted
+  res.rows.forEach(row=>{
+      console.log(row['namakategori']);
+      namakategori[loop++] = row['namakategori'];
+  });
+  await client.end(); 
+}
+x();
 
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
         fs.readFile('./public/index.html', (err, data)=>{
             res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });
+    }
+    else if(req.url.match('\.html$')){
+        let htmlPath = path.join(__dirname, 'public', req.url);
+        fs.readFile(htmlPath, (err, data)=>{
+            res.writeHead(200, {'Content-type': 'text/html'});
             res.end(data);
         });
     }
